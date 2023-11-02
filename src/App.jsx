f062974cebb0ca9cc779076aa2e0208bc5b63817
septianodwikusuma <septianodwik@gmail.com>
2023-11-02 15:38:50 +0700

    feat: dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { Route, Routes } from "react-router-dom"
-import DashboardTiket from "./pages/dashboard/indexTiket"
-import StagingPage from "./pages/staging"
-import DashboardProfile from "./pages/dashboard/indexProfile"
-import TransactionPage from "./pages/transaction"
-
+import { Route, Routes } from "react-router-dom";
+import DashboardTiket from "./pages/dashboard/indexTiket";
+import StagingPage from "./pages/staging";
+import DashboardProfile from "./pages/dashboard/indexProfile";
+import TransactionPageEnded from "./pages/transaction/ended";
+import TransactionPageActive from "./pages/transaction/active";
 
 function App() {
-
   return (
     <>
-    <Routes>
-    <Route path="/dashTiket" element={<DashboardTiket/>}/>
-    <Route path="/dashProfile" element={<DashboardProfile/>}/>
-    <Route path="/" element={<TransactionPage/>}/>
-      
-      
-    </Routes>
+      <Routes>
+        <Route path="/dashTiket" element={<DashboardTiket />} />
+        <Route path="/dashProfile" element={<DashboardProfile />} />
+        <Route path="/checkoutTiket" element={<TransactionPageEnded />} />
+        <Route path="/" element={<TransactionPageActive />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TransactionPage/ContentEnded/tiket.jsx
@@ -0,0 +1,92 @@
+import { Box, Icon, TabPanel, Text } from "@chakra-ui/react";
+import {AiFillClockCircle} from "react-icons/ai"
+import {BsDot} from "react-icons/bs"
+import "./index.css"
+
+const TiketExp = (props) => {
+  return (
+    <>
+        <TabPanel className="boxShadow" backgroundColor={"rgb(245, 247, 250)"} position={"relative"} border={"1px solid rgb(219, 223, 231)"} marginBottom={"-35px"} borderRadius={"8px"} height={"190px"}>
+          <Box display={"flex"} width={"100%"} justifyContent={"center"}>
+            <Box height={"155px"} width={"95%"}>
+              <Box height={"60%"}>
+                <Text
+                  className="judul"
+                  fontSize={"17px"}
+                  height={"40px"}
+                >
+                    {props.judulKeterangan}
+                </Text>
+                <Box
+                  marginTop={"15px"}
+                  gap={"5px"}
+                  display={"flex"}
+                  color={"red"}
+                >
+                  <Icon marginTop={"4px"}>
+                    <AiFillClockCircle size={"sm"} />
+                  </Icon>
+                  <Text>Berakhir 23 August 2023</Text>
+                  <Icon marginTop={"4px"}>
+                    <BsDot size={"xl"} />
+                  </Icon>
+                  <Text>{props.waktu}</Text>
+                </Box>
+              </Box>
+              <Box
+                height={"40%"}
+                justifyContent={"space-between"}
+                borderTop={"1px dashed rgb(219, 223, 231)"}
+                alignItems={"flex-end"}
+                display={"flex"}
+              >
+                <Text
+                  fontWeight={"bold"}
+                  fontSize={"18px"}
+                  marginBottom={"10px"}
+                >
+                    {props.harga}
+                </Text>
+                <Text
+                  fontWeight={"bold"}
+                  fontSize={"16px"}
+                  marginBottom={"10px"}
+                  color={"rgb(255, 59, 48)"}
+                >
+                    {props.status}
+                </Text>
+              </Box>
+            </Box>
+          </Box>
+        </TabPanel>
+        <TabPanel
+          height={"30px"}
+          width={"0px"}
+          position={"relative"}
+          borderRightRadius={"60px"}
+          left={"0px"}
+          top={"-62px"}
+          backgroundColor={"rgb(245, 247, 250)"}
+          borderRight={"1px solid rgb(219, 223, 231)"}
+          borderTop={"1px solid rgb(219, 223, 231)"}
+          borderBottom={"1px solid rgb(219, 223, 231)"}
+        />
+        <TabPanel
+          height={"30px"}
+          width={"0px"}
+          position={"relative"}
+          marginLeft={"95.4%"}
+          borderLeftRadius={"60px"}
+          right={"0px"}
+          top={"-95px"}
+          backgroundColor={"rgb(245, 247, 250)"}
+          borderLeft={"1px solid rgb(219, 223, 231)"}
+          borderTop={"1px solid rgb(219, 223, 231)"}
+          borderBottom={"1px solid rgb(219, 223, 231)"}
+        />
+    
+    </>
+  );
+};
+
+export default TiketExp
