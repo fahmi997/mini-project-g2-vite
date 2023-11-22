587ae52e79d7637aeff33196f8c34e5648f110b0
septianodwikusuma <septianodwik@gmail.com>
2023-11-22 14:00:27 +0700

    feat: berandaNew edit app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-
-import { Route, Routes } from "react-router-dom"
-import DashboardTiket from "./pages/dashboard/indexTiket"
-import LoginPage from "./pages/Login"
-import SignupPage from "./pages/Signup"
-import StagingPage from "./pages/staging"
-import CreateEventPage from "./pages/CreateEvent"
-import DashboardProfile from "./pages/dashboard/indexProfile"
-import TransactionPageEnded from "./pages/transaction/ended"
-import TransactionPageActive from "./pages/transaction/active"
-
+import { Route, Routes } from "react-router-dom";
+import DashboardTiket from "./pages/dashboard/indexTiket";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+import StagingPage from "./pages/staging";
+import CreateEventPage from "./pages/CreateEvent";
+import DashboardProfile from "./pages/dashboard/indexProfile";
+import TransactionPageEnded from "./pages/transaction/ended";
+import TransactionPageActive from "./pages/transaction/active";
 
 function App() {
   return (
