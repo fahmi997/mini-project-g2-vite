3f076330a01f6d57e88d6b82729973f71dbf370a
septianodwikusuma <septianodwik@gmail.com>
2023-11-27 16:59:14 +0700

    Feat: App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,56 @@
 import CreateEventPage from "./pages/CreateEvent";
 import DashboardTiket from "./pages/dashboard/indexTiket";
-// import StagingPage from "./pages/staging";
+import StagingPage from "./pages/staging";
 import DashboardProfile from "./pages/dashboard/indexProfile";
 import TransactionPageEnded from "./pages/transaction/ended";
 import TransactionPageActive from "./pages/transaction/active"
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { checkDataAccount, login } from "../src/redux/slice/accountSlice";
-// import { Route, Routes } from "react-router-dom"
-// import LoginPage from "./pages/Login"
-// import SignupPage from "./pages/Signup"
-// import StagingPage from "./pages/staging"
-// import CreateEventPage from "./pages/CreateEvent"
-// import DashboardProfile from "./pages/dashboard/indexProfile"
-// import TransactionPageEnded from "./pages/transaction/ended"
-// import TransactionPageActive from "./pages/transaction/active"
 import LandingPage from "./pages/LandingPage"
 import ExplorePage from "./pages/ExplorePage.jsx"
 import DetailEvent from "./pages/DetailEventPage"
 import Navbar from "./components/Navbar/index.jsx"
+import BoxCheckout from "./components/TransactionPage/ContentActive/BoxCheckout.jsx";
+// import accountSlice from "./redux/slice/accountSlice";
 
 function App() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation()
   const loginAcc = useSelector((state) => {
     return state.accountSlice;
   });
+  const checkLocal = localStorage.getItem("token")
+
+
+  console.log("INI local storage", localStorage.getItem("token"));
+  if(!checkLocal){
+    navigate("/login")
+  }
 
   useEffect(() => {
     dispatch(checkDataAccount());
-    console.log(loginAcc);
+    console.log("ini loginAcc", loginAcc);
   }, []);
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/explore" element={<ExplorePage />} />
-        <Route path="/event/:id" element={<DetailEvent />} />
+        <Route path="/events/:id" element={<DetailEvent />} />
         <Route path="/dashTiket" element={<DashboardTiket />} />
         <Route path="/dashProfile" element={<DashboardProfile />} />
-        <Route path="/checkoutTiket" element={<TransactionPageEnded />} />
-        {/* <Route path="/" element={<TransactionPageActive />} /> */}
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/sign-up" element={<SignupPage/>}/>
+        <Route path="/checkoutTiketEnded" element={<TransactionPageEnded />} />
         <Route path="/checkoutTiketActive" element={<TransactionPageActive />} />
+        <Route path="/boxCheckout" element={<BoxCheckout />} />
       </Routes>
     </>
   );
