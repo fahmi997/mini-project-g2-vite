e733495af690e454ea2a5312db292177396c01c1
septianodwikusuma <septianodwik@gmail.com>
2023-11-27 16:50:43 +0700

    Feat: pages, LandingPage, index.jsx

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -3,6 +3,9 @@ import CardEvent from "../../components/CardEvent";
 import Carousel from "../../components/Carousel";
 import { useState, useRef, useEffect } from 'react';
 import ButtonScroll from "../../components/ButtonScroll";
+import { API_URL } from "../../helper/helper";
+import axios from "axios";
+
 import { useSelector } from "react-redux";
 // import { selectEventData } from "../../redux/slices/eventSlice";
 import { useDispatch } from "react-redux";
@@ -10,19 +13,27 @@ import { setEventData } from "../../redux/slices/eventSlice";
 import BottomBox from "../../components/BottomBox";
 import FooterMain from "../../components/FooterMain";
 import FooterBottom from "../../components/FooterBottom";
-import API_CALL from "../../helper";
+import Navbar from "../../components/Navbar";
+import NavbarLogIn from "../../components/NavbarLogIn";
 
 const LandingPage = () => {
   const [scrollX, setScrollX] = useState(0);
   const scrollableGridRef = useRef(null);
   const [hideLeft, setHideLeft] = useState(false);
   const [hideRight, setHideRight] = useState(false);
+  const scrollableGridRef2 = useRef(null);
+  const [hideLeft2, setHideLeft2] = useState(false);
+  const [hideRight2, setHideRight2] = useState(false);
+
+  const dataAccount = useSelector((state) => {
+    return state.accountSlice;
+  });
 
   const eventData = useSelector( (state) => {
     return state.eventSlice.eventData
   });
 
-  console.log(eventData);
+  console.log("INI EVENT DATA",eventData);
 
   const dispatch = useDispatch ();  
 
@@ -72,7 +83,7 @@ const LandingPage = () => {
 
   useEffect(() => {
     // Lakukan permintaan ke API untuk mengambil data acara
-    API_CALL.get(`/event`)
+    axios.get(API_URL + `/events`)
     .then ((response) => { 
       dispatch(setEventData(response.data));
     })
@@ -85,6 +96,7 @@ const LandingPage = () => {
 
   return (
     <>
+    {dataAccount.role === ""? <Navbar/> : <NavbarLogIn/>}
       <Flex display={'flex'} flexDirection={'column'} height={'auto'} >
 
         <Carousel />
@@ -161,7 +173,7 @@ const LandingPage = () => {
 
         <Flex ml="5%" mr="5%" mb="6" h="330px" position="relative" overflow="hidden">
           <div
-            ref={scrollableGridRef}
+            ref={scrollableGridRef2}
             style={{
               display: 'flex',
               flexDirection: 'row',
@@ -174,7 +186,7 @@ const LandingPage = () => {
             ))}
           </div>
 
-          <ButtonScroll scrollLeft={() => scrollLeft(scrollableGridRef)} scrollRight={() => scrollRight(scrollableGridRef)} hideLeft={hideLeft} hideRight={hideRight} />
+          <ButtonScroll scrollLeft={() => scrollLeft(scrollableGridRef2)} scrollRight={() => scrollRight(scrollableGridRef2)} hideLeft={hideLeft2} hideRight={hideRight2} />
         </Flex>
 
         <Box mt={'16'} ml={'5%'} mr={'5%'} >
