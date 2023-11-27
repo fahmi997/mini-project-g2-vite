299a30e4989d26e2f9e1070506db912cd680f3b7
septianodwikusuma <septianodwik@gmail.com>
2023-11-27 16:57:59 +0700

    Feat: redux, index.js

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createEvent from "./action/createEvent"
+import accountSlice from "./slice/accountSlice";
 import eventSlice from "./slices/eventSlice";
+import checkoutSlice from "./slice/checkoutSlice";
+import cartSlice from "./slice/cartSlice";
 
 export const globalStore = configureStore({
     reducer:{
         // place reducer here
+        accountSlice,
         createEvent,
-        eventSlice
-        
+        eventSlice,
+        checkoutSlice,
+        cartSlice
     }
 })
